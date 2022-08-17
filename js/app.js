f033ee3eb9ca20a291357637940133fdf4efa56c
NicoFara <nicolas.fara.romero@gmail.com>
2022-08-16 23:49:56 -0300

    Funcion esMayor y mostrarGeneracion agregadas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", mostrarInfo)
+
 // Seleccionar el contenedor de todos los elementos que se agregaran
 let contenedor = document.querySelector("#contenedor");
 
@@ -43,10 +46,12 @@ class Persona {
     this.nacimiento = nacimiento;
   }
 }
+let persona1 = new Persona(nombre, edad, dni, sexo, peso, altura, nacimiento);
 
 //Funcion para mostrar la informacion de la persona
-function mostrarInfo() {
-  let persona1 = new Persona(nombre, edad, dni, sexo, peso, altura, nacimiento);
+function mostrarInfo(e) {
+  e.preventDefault();
+
   let botonInfo = document.getElementById("botonInfo");
 
   //Comprobar si la funcion mostrara la informacion o la ocultara
@@ -106,6 +111,39 @@ function mostrarInfo() {
     botonInfo.classList.remove("btn-ocultar");
 
     contenedorDatos.remove();
+  }
+}
+
+function esMayor() {
+  console.log("hola")
+  if(persona1.edad >= 18) {
+    alert(`Usted tiene ${persona1.edad} años, es mayor de edad`);
+  } else {
+    alert(`Usted tiene ${persona1.edad} años, es menor de edad`);
+  }
+}
+
+function mostrarGeneracion() {
+  if (persona1.nacimiento <= 1948) {
+    alert(
+      `Usted pertenece a la generación Silent generation (Los niños de la posguerra) y su rasgo caracteristico es la austeridad`
+    );
+  } else if (persona1.nacimiento >= 1949 && persona1.nacimiento <= 1968) {
+    alert(
+      `Usted pertenece a la generación Baby Boom y su rasgo caracteristico es la ambición`
+    );
+  } else if (persona1.nacimiento >= 1969 && persona1.nacimiento <= 1980) {
+    alert(
+      `Usted pertenece a la  Generación X y su rasgo caracteristico es la obseción por el éxito`
+    );
+  } else if (persona1.nacimiento >= 1981 && persona1.nacimiento <= 1993) {
+    alert(
+      `Usted pertenece a la  Generación Y (Millennials) y su rasgo caracteristico es la frustación`
+    );
+  } else {
+    alert(
+      `Usted pertenece a la  Generación Z y su rasgo caracteristico es la irreverencia`
+    );
   }
 }
 
